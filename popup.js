43331548df8d70bb1b3a15411aef3fc9427abfed
uma_the390 <ryoma.k.0224@gmail.com>
2023-07-11 23:27:59 +0000

    autocomplete

--- popup.js
+++ popup.js
@@ -1,12 +1,30 @@
-document.getElementById('myForm').addEventListener('submit', function(e) {
+document.getElementById('myForm').addEventListener('submit', function (e) {
     e.preventDefault(); // Stop form from trying to submit
-  
+
     var serviceName = document.getElementById('service').value;
     var projectName = document.getElementById('project').value;
-  
+
     var url = 'https://console.cloud.google.com/' + serviceName + '?project=' + projectName;
-  
+
     // Open the URL in a new tab
     window.open(url, '_blank');
-  });
-  
+});
+
+
+// List of sample services and projects for autoComplete.js
+const services = ["service1", "service2", "service3"];
+const projects = ["project1", "project2", "project3"];
+
+new autoComplete({
+    selector: "#service",
+    data: {
+        src: services,
+    },
+});
+
+new autoComplete({
+    selector: "#project",
+    data: {
+        src: projects,
+    },
+});
