3239c497d174d752529da975cf2f5902c787cc13
uma_the390 <ryoma.k.0224@gmail.com>
2023-07-13 10:13:51 +0000

    fix

--- option/options.js
+++ option/options.js
@@ -25,18 +25,39 @@ function restore_options() {
     chrome.storage.local.get({
         [storageKeyProjectIDs]: [],
     }, function (items) {
-        let projectIDs = []
+        let projectsTable = document.getElementById(htmlKeyProjectsTable);
+        projectsTable.innerHTML = ''; // Clear the table
 
         for (let i = 0; i < items[storageKeyProjectIDs].length; i++) {
             const projectID = items[storageKeyProjectIDs][i];
-            projectIDs.push(`<tr>
-<td>${projectID}</td>
-<td><button onclick="deleteProject('${projectID}')">Delete</button></td>
-</tr>
-            `)
-        }
 
-        document.getElementById(htmlKeyProjectsTable).innerHTML = projectIDs.join('')
+            // Create a new table row and two cells
+            let row = document.createElement('tr');
+            let idCell = document.createElement('td');
+            let actionCell = document.createElement('td');
+
+            // Set the text of the ID cell
+            idCell.textContent = projectID;
+
+            // Create a new button
+            let button = document.createElement('button');
+            button.textContent = "Delete";
+
+            // Add a click event listener to the button
+            button.addEventListener('click', function() {
+                deleteProject(projectID);
+            });
+
+            // Add the button to the action cell
+            actionCell.appendChild(button);
+
+            // Add the cells to the row
+            row.appendChild(idCell);
+            row.appendChild(actionCell);
+
+            // Add the row to the table
+            projectsTable.appendChild(row);
+        }
     });
 }
 
