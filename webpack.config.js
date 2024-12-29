1c20295ba0779216455d1c5730af10517a27c838
ryoma kawahara <ryoma.k.0224@gmail.com>
2024-12-29 22:30:31 +0900

    popup画面が出るところまで

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/popup/popup.html',
-      filename: 'popup.html',
+      filename: 'popup/popup.html',
       chunks: ['popup'],
     }),
     // new HtmlWebpackPlugin({
